1b48fc174fc316a4a7cc240b8bb05ce92bdc067d
mrbertp <aps795@gmail.com>
2021-03-21 15:27:03 +0100

    Added in situ record addition

--- main.py
+++ main.py
@@ -1,47 +1,61 @@
 from tkinter import *
 import config
+import datetime as dt
 
 root = Tk()
 root.title("LabNote")
 root.geometry(f"{config.WIDTH}x{config.HEIGHT}")
 
-record_index = 0
+records = []
 
 
-def add_record(place):
+def refresh_records(master, records):
 
-    global record_index
-    record_index += 1
-    Record(place, r=record_index)
-    print(record_index)
+    for i in range(len(records)):
+        Record(master, i, records[i])
+
+
+def add_record(master, position):
+
+    global records
+    date = dt.datetime.now().strftime("%Y/%m/%d")
+    hour = dt.datetime.now().strftime("%H:%M")
+    text = dt.datetime.now().strftime("%S")
+    content = [date, hour, text]
+    records.insert(position, content)
+    print(records)
+    refresh_records(master, records)
 
 
 class Record():
 
-    def __init__(self, place, r, c=0):
+    def __init__(self, master, position, content):
 
-        record_frame = LabelFrame(place, padx=6, pady=6, bg='grey95', relief=SOLID, bd=2)
-        record_frame.grid(row=r, column=0, sticky=EW)
+        record_frame = LabelFrame(master, padx=6, pady=6, bg='grey95', relief=FLAT, bd=2)
+        record_frame.grid(row=position, column=0, sticky=EW)
 
         record_frame.grid_columnconfigure(0, weight=1)
         record_frame.grid_columnconfigure(1, weight=2)
         record_frame.grid_columnconfigure(2, weight=2)
         record_frame.grid_columnconfigure(3, weight=500)
 
-        top_margin = Button(record_frame, padx=2, pady=2, relief=SOLID, bd=1, bg='grey95')
+        top_margin = Button(record_frame, padx=2, pady=2, relief=SOLID, bd=1, bg='grey95',
+                            command=lambda: add_record(master, position))
         top_margin.grid(row=0, column=0)
         middle_margin = Button(record_frame, padx=2, pady=2, relief=SOLID, bd=1, bg='grey95')
         middle_margin.grid(row=1, column=0)
         bot_margin = Button(record_frame, padx=2, pady=2, relief=SOLID, bd=1, bg='grey95',
-                            command=lambda: add_record(records_frame))
+                            command=lambda: add_record(master, position + 1))
         bot_margin.grid(row=2, column=0)
 
-        date = Button(record_frame, padx=2, pady=2, text=str(record_index), relief=FLAT, bd=1, bg='grey95')
+        date = Button(record_frame, padx=2, pady=2, text=content[0], relief=FLAT, bd=1, bg='grey95')
         date.grid(row=0, column=1)
-        time = Button(record_frame, padx=2, pady=2, text='time', relief=FLAT, bd=1, bg='grey95')
+        time = Button(record_frame, padx=2, pady=2, text=content[1], relief=FLAT, bd=1, bg='grey95')
         time.grid(row=1, column=2)
-        record = Entry(record_frame, bd=2, relief=FLAT)
-        record.grid(row=2, column=3, sticky=EW)
+        text = Entry(record_frame, bd=5, relief=FLAT)
+        text.grid(row=2, column=3, sticky=EW)
+        text.delete(0, END)
+        text.insert(0, content[2])
 
 
 # top bar
@@ -56,7 +70,6 @@ time_label = Label(top_frame, text="Time", bd=2, relief=FLAT, padx=4, pady=4, bg
 time_label.grid(row=0, column=2, sticky=E)
 
 # sections header
-
 headers_frame = Frame(root, padx=4, pady=4, bg='DarkOrange2')
 headers_frame.grid(row=1, column=0, sticky=EW)
 
@@ -68,11 +81,11 @@ planner_header = Button(headers_frame, text='PLANNER', bd=3, relief=RAISED, padx
 planner_header.grid(row=0, column=2)
 
 # records
-
-records_frame = LabelFrame(root, padx=4, pady=4, bd=4, relief=SOLID)
+records_frame = LabelFrame(root, padx=4, pady=4, bd=4, relief=FLAT)
 records_frame.grid(row=2, column=0, sticky=EW)
 records_frame.grid_columnconfigure(0, weight=1)
-record1 = Record(records_frame, r=record_index)
+
+add_record(records_frame, position=0)
 
 
 # row and column config
---
+++ pruebas.py
@@ -0,0 +1,3 @@
+a = [1, 2, 2, 4, 4, 5]
+
+print(reversed(a).index(4))
